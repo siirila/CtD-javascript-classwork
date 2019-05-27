18be514571376768c32da71bc3fe1559150ce495
Jared Siirila <siirila@gmail.com>
2019-05-27 14:43:01 -0400

    Add lodash

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var hello = require("./hello.js");
-const uuidv1 = require('uuid/v1');
+var uuidv1 = require('uuid/v1');
+var _ = require("lodash");
 
 console.log(`Generating uuid: ${uuidv1()}`);
 
-// hello.saySomething();
+var shuffled = _.shuffle([1,2,3,4,5,6,7,8,9,10,11,12,13,14]);
+
+var evenOdd = _.partition(shuffled, function (item ) { return !(item % 2); });
+
+console.log(evenOdd);
